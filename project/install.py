8072a9f608e02f7ffec5d7a44a562c309d4bfae7
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2015-08-29 13:50:10 -0300

    Updated "project".

--- project/install.py
+++ project/install.py
@@ -95,6 +95,8 @@ def clvhealth_jcafb_install():
 
     new_module = install_update_module('clv_address_cst', update)
 
+    new_module = install_update_module('clv_person', update, True)
+
 def secondsToStr(t):
     return "%d:%02d:%02d.%03d" % reduce(lambda ll,b : divmod(ll[0],b) + ll[1:],[(t*1000,),1000,60,60])
 
